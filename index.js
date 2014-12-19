2deff2a65ef30fb3502fe548af4f1c9c4b103609
Camilo Tapia <camilo.tapia@24hr.se>
2014-12-19 18:44:16 +0100

    small fixes

--- index.js
+++ index.js
@@ -2,51 +2,72 @@ var check = require('./check');
 
 var zmq = require('zmq');
 var zonar = require('zonar');
+var Q = require('Q');
 
 // configs
 var port = 5999;
 var address = "tcp://0.0.0.0:" + port;
+var rPort = 5998;
+var rAddress = "tcp://0.0.0.0:" + rPort;
 
 // setup
 var broadcaster = zonar.create({ net: '24hr', name: 'google.ranking' });
-broadcaster.payload = { 'pub': port };
+broadcaster.payload = { 'pub': port, 'req': rPort };
 
+// Create sockets
 var socket = zmq.socket('pub');
+var rSocket = zmq.socket('rep');
 
-var lastPosition = -1;
+// this will hold the last address
+var lastData = { position: -1 };
+
+function getRanking() {
+    console.log('Checking google ranking');
+    if (lastData.position != -1) {
+        return Q.resolve(lastData);
+    } else {
+        return check.find('webbyrå malmö', 'http://www.24hr.se');
+    }
+}
+
+function periodicCheck() {
+
+    getRanking().then(function(result) {
+        lastData = result;
+        socket.send('all' + JSON.stringify(lastData));
+        console.log('Found it on rank %s', result.position);
+    })
+    .then(function() {
+        setTimeout(periodicCheck, 1000 * 60 * 10);
+    });
+
+}
+
+rSocket.bind(rAddress);
+
+rSocket.on('message', function(dataBuffer) {
+    console.log("Got request for current ranking");
+    getRanking().then(function(result) {
+        lastData = result;
+        socket.send('all' + JSON.stringify(lastData));
+        console.log('Send rank %s', lastData.position);
+    });
+});
 
 socket.bind(address, function(err) {
 
-    if (err) throw err;
+    if (err) { 
+        throw err;
+    }
 
     console.log("Google Ranking publishing service started");
 
     broadcaster.start(function() {
-        console.log("Broadcasting...");        
+
+        console.log("Broadcasting...");
+        periodicCheck();
+
     });
-
-    function getPosition() {
-
-        console.log('Checking google ranking');
-
-        check
-            .find('webbyrå malmö', 'http://www.24hr.se')
-            .then(function(result) {
-                if (result.found && lastPosition != result.position) {
-                    lastPosition = result.position;
-                    socket.send('all' + JSON.stringify(result));
-                    console.log('Found it on rank %s', result.position);
-                } else {
-                    console.log('Nothing found on google');
-                }
-            })
-            .then(function() {
-                setTimeout(getPosition, 1000 * 60 * 10);
-            });
-
-    }
-
-    getPosition();
 
 });
 
